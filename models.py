05d4b654aafee6bb60d0fe39efc2e8b91cc7e19c
sametbirol <sametbirol34@gmail.com>
2023-08-06 13:38:43 +0300

    remove adress model

--- models.py
+++ models.py
@@ -13,10 +13,8 @@ class Users(Base):
     hashed_password = Column(String)
     is_active= Column(Boolean, default=True)
     phone_number=Column(String)
-    address_id = Column(Integer,ForeignKey("address.id"),nullable=True)
 
     todos= relationship("Todos", back_populates="owner")
-    address= relationship("Address", back_populates="user_address")
 
 
 class Todos(Base):
@@ -31,15 +29,3 @@ class Todos(Base):
 
     owner = relationship("Users", back_populates="todos")
 
-
-class Address(Base):
-    __tablename__ = 'address'
-    id = Column(Integer, primary_key=True, index=True)
-    address1 = Column(String)
-    address2 = Column(String)
-    city = Column(String)
-    state = Column(String)
-    country = Column(String)
-    postalcode = Column(String)
-    apt_num=Column(String)
-    user_address = relationship("Users",back_populates="address")
