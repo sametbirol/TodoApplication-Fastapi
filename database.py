0f5da70fd506781208c246ef54b206048ebbe252
sametbirol <sametbirol34@gmail.com>
2023-08-06 13:39:03 +0300

    database url comment

--- database.py
+++ database.py
@@ -1,8 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-
+# your SQLAlchemy Database URL go here
+# URL = postgresql://postgres:PASSWORD@localhost/DATABASENAME
 SQLALCHEMY_DATABASE_URL = ""
+
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
